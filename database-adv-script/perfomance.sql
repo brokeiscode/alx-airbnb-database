df440551f7873229786472e5d13842580d47de34
Olakunle Simeon Owolabi <owolabi.olakunlesimeon@gmail.com>
2025-07-01 19:42:01 +0100

    optimization_report commit to database-adv-script v2

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,19 +1,20 @@
 -- initial query that retrieves all bookings along with the user details, property details, and payment details and save it on perfomance.sql
 
-SELECT * 
-FROM bookings b 
-LEFT JOIN users u ON b.user_id = u.user_id 
-LEFT JOIN properties p ON b.property_id = p.property_id
-LEFT JOIN payments pay ON b.booking_id = pay.booking_id
+SELECT *
+FROM bookings b, users u, properties p, payments pay
+WHERE b.user_id = u.user_id
+AND b.property_id = p.property_id
+AND b.booking_id = pay.booking_id;
+
 
 -- Analyze the query’s performance using EXPLAIN and identify any inefficiencies. (12ms)
 -- Planning Time: 3.775 ms
 -- Execution Time: 0.829 ms
-EXPLAIN ANALYZE SELECT * 
-FROM bookings b 
-LEFT JOIN users u ON b.user_id = u.user_id 
-LEFT JOIN properties p ON b.property_id = p.property_id
-LEFT JOIN payments pay ON b.booking_id = pay.booking_id
+EXPLAIN ANALYZE SELECT *
+FROM bookings b, users u, properties p, payments pay
+WHERE b.user_id = u.user_id
+AND b.property_id = p.property_id
+AND b.booking_id = pay.booking_id;
 
 -- Refactor the query to reduce execution time, such as reducing unnecessary joins or using indexing. (5ms)
 -- Planning Time: 0.452 ms
